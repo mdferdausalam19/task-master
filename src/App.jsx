ce47f444e4e33f1046077af727086acc2d4b9dcb
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-06-05 12:18:00 +0600

    feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <div className="bg-[#191D26] font-[Inter] text-white h-screen">
+    <div className="bg-[#191D26] font-[Inter] text-white min-h-screen">
       <Header />
+      <Hero />
       <Footer />
     </div>
   );
