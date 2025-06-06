b434eeedffe269bb9dea064307af54224150529d
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-06-06 17:04:12 +0600

    feat: add TaskBoard component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/task/TaskBoard";
 
 export default function App() {
   return (
     <div className="bg-[#191D26] font-[Inter] text-white min-h-screen">
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </div>
   );
